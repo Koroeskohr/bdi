e9e440fd9df786927afdabd538d741180405321d
Victor Viale <somebodywas@gmail.com>
2015-09-11 18:43:26 +0200

    Added articles link and bootstrap classes to header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,22 +1,27 @@
 <header>
-    <div class="panel">
-        @if (Auth::check())
-            <p>Bienvenue, {{ Auth::user()->name }}</p>
-            <a href="/auth/logout">Logout</a>
-        @else
-            <a href="/login/facebook">Login with Facebook</a>
-        @endif
+    <div class="container">
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="navbar-header">
+                <a href="/" class="navbar-brand">BDI</a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="{{ action('PromotionsController@index') }}">Promotions</a></li>
+                    <li><a href="{{ action('UsersController@index') }}">Elèves</a></li>
+                    <li><a href="{{ action('ArticlesController@index') }}">Articles</a></li>
+                    <li><a href="#">Events</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        @if (Auth::check())
+                            <p>Bienvenue, {{ Auth::user()->name }}</p>
+                            <a href="/auth/logout">Logout</a>
+                        @else
+                            <a href="/login/facebook">Login with Facebook</a>
+                        @endif
+                    </li>
+                </ul>
+            </div>
+        </nav>
     </div>
-    <a href="/"><h1>BDI</h1></a>
-    <nav>
-        <ul>
-            <li><a href="{{ action('PromotionsController@index') }}">Promotions</a></li>
-            <li><a href="{{ action('UsersController@index') }}">Elèves</a></li>
-            <li><a href="#">Articles</a></li>
-            <li><a href="#">Events</a></li>
-        </ul>
-    </nav>
-
-
 </header>
-<hr>
