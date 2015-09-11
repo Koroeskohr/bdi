2788a4785b13c66f6b11661375106547e9068497
Victor Viale <somebodywas@gmail.com>
2015-09-11 18:42:57 +0200

    Added css support

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,14 +4,16 @@
     <meta charset="UTF-8">
     <title>BDI</title>
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    {!! Html::style(asset('css/app.css')) !!}
+
 </head>
 <body>
+    @include('header')
     <div class="container">
-        @include('header')
         @yield('content')
     </div>
     @include('errors/_any')
     @yield('footer')
+    @include('scripts')
 </body>
 </html>
